454272e97a490afc4b07581c5e03eb41839f49fb
Juan Carlos Orrego <jcorrego@gmail.com>
2015-09-09 13:25:53 -0500

    Sendgrid Webhook

--- app/Event.php
+++ app/Event.php
@@ -4,7 +4,23 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Event extends Model
-{
-    //
+class Event extends Model {
+
+    protected $fillable = [
+        'sg_message_id',
+        'email',
+        'time',
+        'category',
+        'event',
+        'url',
+        'asm_group_id',
+        'smtp-id',
+        'useragent',
+        'ip',
+        'status',
+        'reason',
+        'type',
+        'attempt',
+        'response'
+    ];
 }
