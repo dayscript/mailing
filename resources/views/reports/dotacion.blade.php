2cbd338dd589bb88b0b87f072002ede5f3587709
Juan Carlos Orrego <jcorrego@gmail.com>
2015-12-01 12:48:21 -0500

    Report

--- resources/views/reports/dotacion.blade.php
+++ resources/views/reports/dotacion.blade.php
@@ -16,7 +16,7 @@
                 <tr>
                     <td>{{ $count }}</td>
                     <td>{{ $contact->email }}</td>
-                    <td>{{ $contact->lastEvent()->event }}</td>
+                    <td>{{ $contact->lastEvent()}}</td>
                 </tr>
                 <?php $count++;?>
             @endforeach
