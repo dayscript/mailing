eb8fac8b789e87c9ebef791ec6fa81aa8c04dba5
Juan Carlos Orrego <jcorrego@gmail.com>
2015-12-01 08:10:40 -0500

    Dotacion test

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -7,7 +7,7 @@
         <div class="row">
             <div class="col col-md-6 col-md-offset-3">
                 <div class="panel panel-default">
-                    <div class="panel-heading"><h3 class="panel-title">Enviar campaña Navidad</h3></div>
+                    <div class="panel-heading"><h3 class="panel-title">Enviar campaña</h3></div>
                     <div class="panel-body">
                         {!! Form::open(['route' => 'dotacion', 'method' => 'post']) !!}
                         <div class="form-group">
