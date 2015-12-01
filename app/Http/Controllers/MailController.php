50fcaa660a951c8902c2b294859963daa5ed603c
Juan Carlos Orrego <jcorrego@gmail.com>
2015-12-01 13:11:54 -0500

    Report

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -29,7 +29,7 @@ class MailController extends Controller {
 //        $contacts = Contact::where('dotacion1',1)->limit(10000)->get();
         $contacts = DB::table('contacts')
             ->leftJoin('events', 'contacts.email', '=', 'events.email')
-            ->select('contacts.name', 'contacts.email')
+            ->select('contacts.name', 'contacts.email','events.event')
             ->where('contacts.dotacion1',1)
             ->limit(10)
             ->get();
