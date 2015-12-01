40ce097b2232e610655f0e333726bc8bed58957c
Juan Carlos Orrego <jcorrego@gmail.com>
2015-12-01 12:51:02 -0500

    Report

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -25,7 +25,7 @@ class MailController extends Controller {
 
     public function report()
     {
-        $contacts = Contact::where('dotacion1',1)->limit(2)->get();
+        $contacts = Contact::where('dotacion1',1)->limit(20)->get();
 
         return view('reports.dotacion',compact('contacts'));
     }
