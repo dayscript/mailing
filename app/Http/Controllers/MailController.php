63af3d82e3bc803609e1fbc91a57267cf6d0a76f
Juan Carlos Orrego <jcorrego@gmail.com>
2015-12-01 13:13:38 -0500

    Report

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -31,7 +31,7 @@ class MailController extends Controller {
             ->leftJoin('events', 'contacts.email', '=', 'events.email')
             ->select('contacts.name', 'contacts.email','events.event','events.created_at')
             ->where('contacts.dotacion1',1)
-            ->orderBy('events.created_at','desc')
+//            ->orderBy('events.created_at','desc')
             ->limit(10)
             ->get();
         dd($contacts);
