2ff439722b3a3070fea7d760134192e7d10defd5
Juan Carlos Orrego <jcorrego@gmail.com>
2016-03-15 11:32:29 -0500

    template link

--- resources/views/emails/soydt2016.blade.php
+++ resources/views/emails/soydt2016.blade.php
@@ -61,7 +61,7 @@
                 <table class="main" width="100%" cellpadding="0" cellspacing="0" style="font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; border-radius: 3px; background: #fff; margin: 0; border: 1px solid #e9e9e9;">
                     <tr style="font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
                         <td class="content-wrap" style="font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 20px;" valign="top">
-                            <a href="http://www.navidadsodexo.com" target="_blank">
+                            <a href="http://www.soydt.com" target="_blank">
                                 <img width="100%;" src="<?php echo $message->embed( public_path() . "/mails/soydt/soydt2016.jpg" ); ?>"
                                      alt="">
                             </a>
